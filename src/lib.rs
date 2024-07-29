ca1de5a0bb394d17d923937d0459c8635428c060
Mark Wotton <mwotton@gmail.com>
2024-07-29 09:38:56 +0700

    package 0.1.1 properly

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,15 @@
-
-
 // We want to make sure we are getting the shortest match possible
 // without getting tripped up by pathological cases.
 pub mod minspan {
 
-    pub fn span<A>(query: &Vec<A>, history : &Vec<A>) -> (usize,usize)
+    pub fn span<A>(query: &Vec<A>, history : &Vec<A>) -> Option<(usize,usize)>
 	where
     	A: PartialEq {
 
 	let mut starting_at : Vec<Option<(usize,usize)>> = query.iter().map(|_| None).collect();
 	let mut best_complete_solution : Option<(usize,usize)> = None;
 
-	//	println!("debugging source starting_at {:?}", blah);
-
-	if query.len() == 0 { return (0,0); }
+	if query.len() == 0 { return Some((0,0)); }
 
 	for (bodyindex, bodychr) in history.iter().enumerate() {
 	    for (keyindex, keychr) in query.iter().enumerate() {
@@ -69,14 +65,7 @@ pub mod minspan {
 		}
 	    }
 	}
-
-	// this is a little unfortunate: when we are asked to match a query that is found nowhere, we don't want to return a None,
-	// as the comparison behaviour is wrong. therefore, we'll return a set of indices that are one larger than the longest
-	// possible legitimate match.
-	match best_complete_solution {
-	    Some(x) => x,
-	    None => (0,history.len()),
-	}
+	best_complete_solution
     }
 }
 
@@ -87,19 +76,19 @@ mod tests {
     #[test]
     fn test_minimal_match() {
 	let wrapper = |needle: &str,haystack: &str| {
-	    let (from,to) = minspan::span( &needle.chars().collect(),
-					    &haystack.chars().collect());
-	    println!("to {}, from {}", to,from);
-	    1+to-from
+	    match minspan::span( &needle.chars().collect(),
+				  &haystack.chars().collect()) {
+		Some ((from,to)) => Some(1+to-from),
+		None => None,
+	    }
 	};
 
-	assert_eq!(wrapper("ab", "ab"), 2);
-	assert_eq!(wrapper("a", "ab"), 1);
-	assert_eq!(wrapper("ab", "abc"), 2);
-	assert_eq!(wrapper("abc", "abcd"), 3);
-	assert_eq!(wrapper("curl", "curly"), 4);
-	assert_eq!(wrapper("curl", "acccccurlycurrelly"), 4);
-	// one past the end
-	assert_eq!(wrapper("z", "acccccurlycurrelly"), 19);
+	assert_eq!(wrapper("ab", "ab").unwrap(), 2);
+	assert_eq!(wrapper("a", "ab").unwrap(), 1);
+	assert_eq!(wrapper("ab", "abc").unwrap(), 2);
+	assert_eq!(wrapper("abc", "abcd").unwrap(), 3);
+	assert_eq!(wrapper("curl", "curly").unwrap(), 4);
+	assert_eq!(wrapper("curl", "acccccurlycurrelly").unwrap(), 4);
+	assert_eq!(wrapper("z", "acccccurlycurrelly"), None);
     }
 }
